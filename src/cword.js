33909e297b0f06afcd00e94b052b2f5b447f539e
Richard Williams <1234richardwilliams@gmail.com>
2024-09-20 09:20:05 +0100

    Update

--- src/cword.js
+++ src/cword.js
@@ -4,24 +4,41 @@ let crosswordList;
 let crosswordObject = {};
 let limits;
 
-const fetchTimeout = (url, ms, { signal, ...options } = {}) => {
-	const controller = new AbortController();
-	const promise = fetch(url, { signal: controller.signal, ...options });
-	if (signal) signal.addEventListener('abort', () => controller.abort());
-	const timeout = setTimeout(() => controller.abort(), ms);
-	return promise.finally(() => clearTimeout(timeout));
-};
+// const fetchTimeout = (url, ms, { signal, ...options } = {}) => {
+// 	const controller = new AbortController();
+// 	const promise = fetch(url, { signal: controller.signal, ...options });
+// 	if (signal) signal.addEventListener('abort', () => controller.abort());
+// 	const timeout = setTimeout(() => controller.abort(), ms);
+// 	return promise.finally(() => clearTimeout(timeout));
+// };
 
 const getCrosswordPromise = (type, id) => {
 	console.log(`Fetching ${type} ${id}`);
-	return fetchTimeout(`https://www.theguardian.com/crosswords/${type}/${id}`, 3000)
-		.then((x) => x.text())
+	const options = {
+		headers: {
+			// Host: 'www.theguardian.com',
+			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0',
+			// DNT: 1,
+			'Sec-GPC': 1,
+			// Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8',
+			'Accept-Language': 'en-GB,en;q=0.5',
+			// 'Accept-Encoding': 'gzip, deflate, br, zstd',
+			Connection: 'keep-alive',
+		},
+	};
+	return fetch(`https://www.theguardian.com/crosswords/${type}/${id}`, options)
+		.then((x) => {
+			return x.text();
+		})
 		.then((html) => {
 			console.log(`Got ${type} ${id}`);
 			return html;
 		})
 		.then((html) => JSON.parse(decode(html.match(/data-crossword-data="([^"]+)"/)[1])))
-		.catch(() => ({}));
+		.catch((e) => {
+			console.log(`Error fetching ${type}, ${id}`);
+			console.log(e);
+		});
 };
 
 async function getBits(env) {
